12b5251fdb4c81dec9c043dc79734d2f7a22d9d9
alex <alex.qiubo@qq.com>
2017-06-24 23:27:30 +0800

    delete unuseful file

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,8 +3,8 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=zcg',
-            'username' => 'root',
+            'dsn' => 'mysql:host=localhost;dbname=zcg',
+            'username' => 'alex',
             'password' => 'woshishei',
             'charset' => 'utf8',
         ],
