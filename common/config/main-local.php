cce0270c7759efb7b23a657f3194f0b975f9d244
alex <alex.qiubo@qq.com>
2017-06-25 00:14:05 +0800

    debug

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=zcg',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=zcg;port=3306',
             'username' => 'root',
             'password' => 'woshishei',
             'charset' => 'utf8',
