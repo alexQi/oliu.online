0b2ebd142d825ad8dcacaff1d3ccbc3b82d70b45
alex <alex.qiubo@qq.com>
2017-06-25 12:14:27 +0800

    .......

--- wechat/controllers/IndexController.php
+++ wechat/controllers/IndexController.php
@@ -12,11 +12,11 @@ class IndexController extends Controller
 {
     public function actionIndex(){
         $fileContent = file_get_contents("php://input");
-        yii::info($fileContent,'wechat.message');
         libxml_disable_entity_loader(true);
         $data = simplexml_load_string($fileContent, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $data = json_encode($data);
         yii::info($data,'wechat.message');
-        $data = json_decode(json_encode($data), true);
+        $data = json_decode($data, true);
 
 
     }
