650e546053bc484c8cb843237ca7fefb63b7a1ae
alex <alex.qiubo@qq.com>
2017-07-02 21:45:36 +0800

    debug

--- backend/views/api/update.php
+++ backend/views/api/update.php
@@ -7,12 +7,12 @@ use yii\helpers\Html;
 
 $this->title = 'Update Api Base: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Api Bases', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->api_name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="api-base-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<!--    <h1>--><?php //echo Html::encode($this->title) ?><!--</h1>-->
 
     <?= $this->render('_form', [
         'model' => $model,
