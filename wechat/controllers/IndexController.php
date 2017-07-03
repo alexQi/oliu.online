cd4d1b2a0b2b4564aac69160fbbf526568db1b82
qiubo <alex.qiubo@qq.com>
2017-07-03 15:00:07 +0800

    debug

--- wechat/controllers/IndexController.php
+++ wechat/controllers/IndexController.php
@@ -29,6 +29,9 @@ class IndexController extends Controller
     }
 
     public function actionTest(){
+        $wechat = new Wechat();
+        var_dump($wechat->getAccessToken());
+
         var_dump($this->api->run());
     }
 
