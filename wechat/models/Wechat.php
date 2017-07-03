13339911d6e7d6ccd5cddd1ab36d5a555a39571d
qiubo <alex.qiubo@qq.com>
2017-07-03 14:57:58 +0800

    debug

--- wechat/models/Wechat.php
+++ wechat/models/Wechat.php
@@ -11,6 +11,7 @@ namespace wechat\models;
 use Yii;
 use yii\base\Model;
 use common\models\Api;
+use common\components\Common;
 
 class Wechat extends Model{
 
@@ -49,6 +50,14 @@ class Wechat extends Model{
         }else{
             return false;
         }
+    }
+
+    public function getAccessToken(){
+        $param['grant_type'] = 'client_credential';
+        $param['appid']      = yii::$app->params['wechat']['AppID'];
+        $param['secret']     = yii::$app->params['wechat']['AppSecret'];
+        $res = Common::httpRequest(yii::$app->params['wechat']['getToken'],$param,'post');
+        return json_decode($res);
     }
 
 
