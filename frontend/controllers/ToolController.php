57f1475d348b63d4ba66ab98b922e58163aa0608
qiubo <alex.qiubo@qq.com>
2017-07-11 17:05:41 +0800

    modify

---
+++ frontend/controllers/ToolController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 17-7-11
+ * Time: 下午2:46
+ */
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class ToolController extends Controller
+{
+    //重置全局layout
+    public $layout = '';
+
+    public function actionJsonFormat()
+    {
+
+    }
+}
