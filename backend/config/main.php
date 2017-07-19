ce6f79af17c709d96804a6b57ef3a2330e9e6674
qiubo <alex.qiubo@qq.com>
2017-07-19 16:01:20 +0800

    modify

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,11 +14,8 @@ return [
     'language'=>'zh-CN',
     "modules" => [
         "admin" => [
-            "class" => "mdm\admin\Module",
+            "class" => "backend\modules\admin\Module",
         ],
-    ],
-    "aliases" => [
-        "@mdm/admin" => "@vendor/mdmsoft/yii2-admin",
     ],
     'components' => [
         'request' => [
@@ -71,7 +68,7 @@ return [
         ],
     ],
     'as access' => [
-        'class' => 'mdm\admin\components\AccessControl',
+        'class' => 'backend\modules\admin\components\AccessControl',
         'allowActions' => [
             //这里是允许访问的action
             //controller/action
