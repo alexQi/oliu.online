0856fa6e111afbb3308210f44b3639af399296e3
qiubo <alex.qiubo@qq.com>
2017-08-18 17:04:18 +0800

    modify

--- console/controllers/ConsController.php
+++ console/controllers/ConsController.php
@@ -46,6 +46,34 @@ class ConsController extends Controller
         }
     }
 
+    public function actionSaveImessage()
+    {
+        $beanstalk = new Beanstalk();
+        $beanstalk->useTube('oliu.sendEmail');
+        $beanstalk->watch('oliu.sendEmail');
+
+        while (true){
+            $job  = $beanstalk->reserve();
+            try{
+                $data = $job->getData();
+
+                $param = json_decode($data,true);
+
+                $mail = new Mail();
+                $res = $mail->SendMail($param);
+
+                if (!$res){
+                    throw new Exception('发送失败');
+                }
+                $beanstalk->delete($job);
+            }catch (Exception $e)
+            {
+                $beanstalk->release($job,5,10);
+                echo $e->getMessage();
+            }
+        }
+    }
+
     public function actionInvokeWork(){
         //初始化Api
         $api = new Api();
